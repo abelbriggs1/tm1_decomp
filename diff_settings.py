31f6593ea436133b44ee0f640d8aaf02ae250702
Abel Briggs <abelbriggs1@hotmail.com>
2024-05-19 21:07:05 +0000

    Fix diff settings, update `m2c` so it can be run standalone

--- diff_settings.py
+++ diff_settings.py
@@ -2,6 +2,6 @@ def apply(config, args):
     config["arch"] = "mipsel"
     config["baseimg"] = "disks/SIPS_600.07"
     config["myimg"] = "build/TM1.EXE"
-    config["mapfile"] = "TM1.map"
+    config["mapfile"] = "build/TM1.map"
     config["source_directories"] = ["."]
     config["objdump_executable"] = "mipsel-linux-gnu-objdump"
